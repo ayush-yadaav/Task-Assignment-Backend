7fef52ce5811a6f5b6fa218c575cf65542baeced
ayush-yadaav <yadavayush2091a@gmail.com>
2026-02-18 11:53:43 +0530

    slove

--- app.js
+++ app.js
@@ -1,51 +1,4 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import morgan from "morgan";
-// import helmet from "helmet";
-// import authRoutes from "./routes/auth.routes.js";
-// import taskRoutes from "./routes/task.routes.js";
 
-// dotenv.config();
-
-// const app = express();
-
-// app.use(express.json());
-// // app.use(cors(
-// //     {
-// //         origin: "https://task-assignment-gilt.vercel.app/",
-// //         credentials: true
-// //     }
-// // ));
-
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "http://localhost:3000",
-//   "https://task-assignment-gilt.vercel.app"
-// ];
-
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin) return callback(null, true); // Postman allow
-
-//       if (allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
-
-// app.use(helmet());
-// app.use(morgan("dev"));
-
-// app.use("/api/v1/auth", authRoutes);
-// app.use("/api/v1/tasks", taskRoutes);
-
-// export default app;
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -58,7 +11,7 @@ dotenv.config();
 
 const app = express();
 
-// ✅ CORS FIRST (before routes)
+
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
@@ -76,7 +29,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
 
-// Optional root route (avoid Cannot GET /)
+
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
